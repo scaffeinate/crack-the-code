1db8134971c236056500eaed05dda38d31403d8d
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-02-28 03:45:03 -0600

    Question 1.6 added my solution

---
+++ src/chapter_1/StringCompression.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package chapter_1;
+
+import java.util.Scanner;
+
+/**
+ * Question 1.6: Implement a method to perform basic string compression using
+ * the counts of repeated characters. For example, the string aabcccccaaa would
+ * become a2b1c5a3. If the "compressed" string would not become smaller than the
+ * original string, your method should return the original string. You can
+ * assume the string has only uppercase and lowercase letters (a-z).
+ * 
+ * @author Sudharsanan Muralidharan
+ */
+public class StringCompression {
+
+  private String compressString(String input) {
+    StringBuilder builder = new StringBuilder();
+    int i = 0, counter = 1;
+
+    for (i = 0; i < input.length() - 1; i++) {
+      if (input.charAt(i) == input.charAt(i + 1)) {
+        counter++;
+      } else {
+        builder.append(input.charAt(i)).append(counter);
+        counter = 1;
+      }
+    }
+
+    builder.append(input.charAt(i)).append(counter);
+
+    return (builder.length() < input.length()) ? builder.toString() : input;
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Scanner scanner = new Scanner(System.in);
+    StringCompression stringCompression = new StringCompression();
+
+    String input = scanner.nextLine();
+    if (input != null && !input.isEmpty()) {
+      System.out.println(stringCompression.compressString(input));
+    }
+
+    scanner.close();
+  }
+
+}
