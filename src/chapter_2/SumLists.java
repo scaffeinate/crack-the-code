b59a62c89f6122b1b6ed482d141942d8c09a1b5c
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2016-03-07 18:40:00 -0600

    Question 2.5 test for all cases

--- src/chapter_2/SumLists.java
+++ src/chapter_2/SumLists.java
@@ -23,30 +23,62 @@ import java.util.Scanner;
  */
 public class SumLists {
 
+  /**
+   * Returns the sum of digits from the linked lists. Note that the digits are
+   * stored in reverse.
+   * 
+   * @param list
+   * @param list2
+   * @return sum
+   */
   private int computeSum(CustomLinkedList<Integer> list, CustomLinkedList<Integer> list2) {
     int size1 = list.size();
     int size2 = list2.size();
 
+    /*
+     * to handle length mismatch, fetch the larger and smaller lists head.
+     * current node points the head of the larger linked list current2 points
+     * the other list.
+     */
     Node<Integer> current = size1 > size2 ? list.head() : list2.head();
     Node<Integer> current2 = size1 > size2 ? list2.head() : list.head();
 
-    return computeSum(current, current2, 0, 0);
+    return add(current, current2, 0, 0);
   }
 
-  private int computeSum(Node<Integer> current, Node<Integer> current2, int carry, int place) {
+  /**
+   * Recursive function which computes the sum of digits
+   * 
+   * @param current
+   * @param current2
+   * @param carry
+   * @param place
+   * @return sum
+   */
+  private int add(Node<Integer> current, Node<Integer> current2, int carry, int place) {
 
     int total = 0, sum = 0;
 
+    /*
+     * current2 points to the smaller list and current to the larger. Add the
+     * digits till current2 runs out and then compute the rest of the digits for
+     * current
+     */
     if (current2 != null) {
+      // compute the sum of digits + carryover
       sum = current.data + current2.data + carry;
+      // compute new carryover
       carry = sum / 10;
-      total += Math.pow(10, place) * (sum % 10)
-          + computeSum(current.next, current2.next, carry, ++place);
+      // (10^place) * digit + add() of rest of the digits
+      total += Math.pow(10, place) * (sum % 10) + add(current.next, current2.next, carry, ++place);
     } else if (current != null) {
+      // since the smaller list digits are exhausted add digit + carryover
       sum = current.data + carry;
+      // compute new carryover
       carry = sum / 10;
-      total += Math.pow(10, place) * (sum % 10)
-          + computeSum(current.next, null, carry, ++place);
+      total += Math.pow(10, place) * (sum % 10) + add(current.next, null, carry, ++place);
+    } else {
+      total += Math.pow(10, place) * carry;
     }
 
     return total;
@@ -59,9 +91,11 @@ public class SumLists {
     String input;
     String[] elements, elements2;
     CustomLinkedList<Integer> list, list2;
+    StringBuilder builder;
 
     while (scanner.hasNextLine()) {
       input = scanner.nextLine();
+      builder = new StringBuilder();
       list = new CustomLinkedList<Integer>();
       list2 = new CustomLinkedList<Integer>();
 
@@ -70,13 +104,19 @@ public class SumLists {
 
       for (String e : elements) {
         list.add(Integer.parseInt(e));
+        builder.append(e);
       }
+
+      builder.append('+');
 
       for (String e : elements2) {
         list2.add(Integer.parseInt(e));
+        builder.append(e);
       }
 
-      System.out.println(sumLists.computeSum(list, list2));
+      System.out.print(builder.reverse().toString() + "=");
+      System.out.print(sumLists.computeSum(list, list2));
+      System.out.println();
     }
 
     scanner.close();
---
+++ src/chapter_2/SumListsForward.java
@@ -0,0 +1,24 @@
+package chapter_2;
+
+/**
+ * Question 2.5: You have two numbers represented by a linked list, where each
+ * node contains a single digit. The digits are stored in reverse order, such
+ * that the 1's digit is at the head of the list. Write a function that adds the
+ * two numbers and returns the sum as a linked list.
+ * 
+ * FOLLOW UP
+ * 
+ * Suppose the digits are stored in forward order. Repeat the above problem.
+ * 
+ * @author Sudharsanan Muralidharan
+ */
+public class SumListsForward {
+
+  private int computeSum(CustomLinkedList<Integer> list, CustomLinkedList<Integer> list2) {
+    return -1;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
