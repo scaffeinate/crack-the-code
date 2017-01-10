b98a1115a1066db046f0d2275b9a58c2a33e555b
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-01-10 02:19:00 -0500

    TODO: Need to finish the solution for 3.3

---
+++ cracking-the-coding-interview/src/chapter_3/SetOfPlates.java
@@ -0,0 +1,13 @@
+package chapter_3;
+
+/**
+ * Question 3.3: Imagine a (literal) stack of plates. If the stack gets too high
+ * it might topple.
+ * 
+ * @author Sudharsanan Muralidharan
+ */
+public class SetOfPlates {
+  public static void main(String[] args) {
+    
+  }
+}
