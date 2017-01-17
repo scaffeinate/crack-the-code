2a01ca7b2b724e8eb3cfc09e8acd87b403755e75
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-01-16 20:52:49 -0500

    Finish up remove in HashTable

--- datastructures/src/datastructures/hashtable/HashTable.java
+++ datastructures/src/datastructures/hashtable/HashTable.java
@@ -17,7 +17,7 @@ public class HashTable<K, V> {
   public HashTable() {
     hashArray = (HashTableNode<K, V>[]) Arrays.copyOf(new Object[hashSize], hashSize, HashTableNode[].class);
   }
-  
+
   public void put(K key, V value) {
     int index = hash(key);
     HashTableNode<K, V> head = hashArray[index];
@@ -25,14 +25,41 @@ public class HashTable<K, V> {
   }
 
   public V get(K key) {
-     int index = hash(key);
-     HashTableNode<K, V> node = find(hashArray[index], key);
-     return (node != null) ? node.value : null;
+    int index = hash(key);
+    HashTableNode<K, V> node = find(hashArray[index], key);
+    return (node != null) ? node.value : null;
   }
-  
+
+  public void remove(K key) {
+    int index = hash(key);
+    HashTableNode<K, V> node = hashArray[index];
+    if (node == null) {
+      return;
+    } else if (node.key.equals(key)) {
+      hashArray[index] = node.next;
+      size--;
+    } else {
+      delete(node, key);
+    }
+  }
+
+  public void delete(HashTableNode<K, V> head, K key) {
+    HashTableNode<K, V> current = head;
+    HashTableNode<K, V> runner = head.next;
+
+    while (runner != null) {
+      if (runner.key.equals(key)) {
+        current.next = runner.next;
+        size--;
+      }
+      current = current.next;
+      runner = runner.next;
+    }
+  }
+
   private HashTableNode<K, V> add(HashTableNode<K, V> head, K key, V value) {
     HashTableNode<K, V> node = find(head, key);
-    if(node != null) {
+    if (node != null) {
       node.value = value;
     } else {
       node = new HashTableNode<K, V>(key, value);
@@ -40,23 +67,20 @@ public class HashTable<K, V> {
       head = node;
       size++;
     }
-    
     return head;
   }
-  
+
   private HashTableNode<K, V> find(HashTableNode<K, V> head, K key) {
     HashTableNode<K, V> current = head;
     while (current != null) {
       if (current.key.equals(key)) {
         return current;
       }
-      
       current = current.next;
     }
-
     return null;
   }
-  
+
   private int hash(K key) {
     return (key.hashCode() % hashSize);
   }
