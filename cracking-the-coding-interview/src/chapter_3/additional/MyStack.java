8573eda2768be8e569c8137d37b91447993b6922
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-01-17 01:29:41 -0500

    Add Stack using Queue implementations

---
+++ cracking-the-coding-interview/src/chapter_3/additional/MyStack.java
@@ -0,0 +1,77 @@
+package chapter_3.additional;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import datastructures.stack.GenericStack;
+
+public class MyStack<T> implements GenericStack<T> {
+  private Queue<T> firstQueue, secondQueue;
+  boolean pushingToFirstQueue = true;
+  private int size = 0;
+  
+  public MyStack() {
+    firstQueue = new LinkedList<T>();
+    secondQueue = new LinkedList<T>();
+  }
+
+  @Override
+  public T peek() {
+    T data = null;
+    if(pushingToFirstQueue) {
+      data = transfer(firstQueue, secondQueue);
+      secondQueue.add(data);
+    } else {
+      data = transfer(secondQueue, firstQueue);
+      firstQueue.add(data);
+    }
+    pushingToFirstQueue = !pushingToFirstQueue;
+    return data;
+  }
+
+  @Override
+  public void push(T data) {
+    if(pushingToFirstQueue) {
+      firstQueue.add(data);
+    } else {
+      secondQueue.add(data);
+    }
+    
+    size++;
+  }
+
+  @Override
+  public T pop() {
+    T data = null;
+    if(pushingToFirstQueue) {
+      data = transfer(firstQueue, secondQueue);
+    } else {
+      data = transfer(secondQueue, firstQueue);
+    }
+    size--;
+    pushingToFirstQueue = !pushingToFirstQueue;
+    return data;
+  }
+  
+  private T transfer(Queue<T> queue1, Queue<T> queue2) {
+    T data = null;
+    int queueSize = queue1.size();
+    while(queueSize-- > 0) {
+      data = queue1.remove();
+      if(queueSize != 0) {
+        queue2.add(data);
+      }
+    }
+    return data;
+  }
+
+  @Override
+  public int size() {
+    return this.size;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return (this.size == 0);
+  }
+}
---
+++ cracking-the-coding-interview/src/chapter_3/additional/MyStack2.java
@@ -0,0 +1,64 @@
+package chapter_3.additional;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import datastructures.stack.GenericStack;
+
+public class MyStack2<T> implements GenericStack<T> {
+
+  private Queue<T> firstQueue;
+  private Queue<T> secondQueue;
+  private boolean pushingToFirstQueue = true;
+  private int size = 0;
+
+  public MyStack2() {
+    firstQueue = new LinkedList<T>();
+    secondQueue = new LinkedList<T>();
+  }
+
+  @Override
+  public void push(T data) {
+    if(pushingToFirstQueue) {
+      firstQueue.add(data);
+      transfer(secondQueue, firstQueue);
+    } else {
+      secondQueue.add(data);
+      transfer(firstQueue, secondQueue);
+    }
+    size++;
+    pushingToFirstQueue = !pushingToFirstQueue;
+  }
+
+  @Override
+  public T pop() {
+    size--;
+    return current().poll();
+  }
+
+  @Override
+  public T peek() {
+    return current().peek();
+  }
+
+  @Override
+  public int size() {
+    return this.size;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return this.size == 0;
+  }
+  
+  private void transfer(Queue<T> queue1, Queue<T> queue2) {
+    while(queue1.size() > 0) {
+      queue2.add(queue1.remove());
+    }
+  }
+  
+  private Queue<T> current() {
+    Queue<T> currentQueue = (pushingToFirstQueue) ? secondQueue : firstQueue;
+    return currentQueue;
+  }
+}
