b47a096c48089d5e2ff53886106cdb8835a3def6
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-01-26 02:27:18 -0500

    Finish easy on to end today’s session

--- hackerrank/src/algorithms/implementation/ViralAdvertising.java
+++ hackerrank/src/algorithms/implementation/ViralAdvertising.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 /**
  * Challenge: https://www.hackerrank.com/challenges/strange-advertising
+ * 
  * @author Sudharsanan Muralidharan
  */
 public class ViralAdvertising {
@@ -16,11 +17,11 @@ public class ViralAdvertising {
     int initial = 5;
     int sum = 0;
     for (int i = 1; i <= n; i++) {
-      initial = (int) (Math.floor(initial/2));
+      initial = (int) (Math.floor(initial / 2));
       sum += (initial);
       initial *= 3;
     }
-    
+
     System.out.println(sum);
     in.close();
   }
