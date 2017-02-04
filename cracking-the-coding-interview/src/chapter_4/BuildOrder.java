8e32b970f6d1a424e9dbc456f5c73b1d40799150
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-02-04 01:01:42 -0500

    Removed sorting, Added Book example graph

--- cracking-the-coding-interview/src/chapter_4/BuildOrder.java
+++ cracking-the-coding-interview/src/chapter_4/BuildOrder.java
@@ -5,8 +5,6 @@ package chapter_4;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -53,44 +51,39 @@ public class BuildOrder {
   }
 
   private String fetchBuildOrder() {
-    Collections.sort(projects, new Comparator<Vertex<Character>>() {
-      @Override
-      public int compare(Vertex<Character> o1, Vertex<Character> o2) {
-        return Integer.valueOf(o1.outgoingEdges.size()).compareTo(Integer.valueOf(o2.outgoingEdges.size()));
-      }
-    });
-    
     Set<Vertex<Character>> visitedSet = new HashSet<Vertex<Character>>();
     StringBuilder builder = new StringBuilder();
-    for(Vertex<Character> project:projects) {
-      if(project.outgoingEdges.size() == 0) {
-        visitedSet.add(project);
-        builder.append(project.label).append(" ");
-      } else {
-        if(!visitedSet.contains(project)) {
+    for (Vertex<Character> project : projects) {
+      if (!visitedSet.contains(project)) {
+        if (project.outgoingEdges.size() == 0) {
+          visitedSet.add(project);
+          builder.append(project.label).append(" ");
+        } else {
           depthFirstTraversal(project, builder, visitedSet);
         }
       }
     }
-    
+
     return builder.toString();
   }
-  
-  private void depthFirstTraversal(Vertex<Character> sourceVertex, StringBuilder builder, Set<Vertex<Character>> visitedSet) {
-    if(sourceVertex == null) {
+
+  private void depthFirstTraversal(Vertex<Character> sourceVertex, StringBuilder builder,
+      Set<Vertex<Character>> visitedSet) {
+    if (sourceVertex == null) {
       return;
     }
-    
-    visitedSet.add(sourceVertex);
-    
+
     List<Vertex<Character>> neighbours = graph.neighboursOf(sourceVertex);
-    for(Vertex<Character> vertex : neighbours) {
-      if(!visitedSet.contains(vertex)) {
+    for (Vertex<Character> vertex : neighbours) {
+      if (!visitedSet.contains(vertex)) {
         depthFirstTraversal(vertex, builder, visitedSet);
       }
     }
-    
-    builder.append(sourceVertex.label).append(" ");
+
+    if (!visitedSet.contains(sourceVertex)) {
+      visitedSet.add(sourceVertex);
+      builder.append(sourceVertex.label).append(" ");
+    }
   }
 
   public static void main(String[] args) throws FileNotFoundException {
@@ -99,11 +92,11 @@ public class BuildOrder {
     buildOrder.constructGraph(input);
     System.out.println(buildOrder.fetchBuildOrder());
   }
-  
+
   class Project {
     char label;
     int numDependencies = 0;
-    
+
     public Project(char label, int numDependencies) {
       this.label = label;
       this.numDependencies = numDependencies;
