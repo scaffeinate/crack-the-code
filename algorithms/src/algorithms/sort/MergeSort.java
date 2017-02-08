cd374f1ba9e6a6ea85d1a63e4b0a8cf4f93cbfc6
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-02-07 19:28:15 -0500

    Added a generic implementation of MergeSort

---
+++ algorithms/src/algorithms/sort/MergeSort.java
@@ -0,0 +1,69 @@
+package algorithms.sort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MergeSort<T> {
+  private Class<?> clazz;
+
+  public MergeSort(Class<?> clazz) {
+    this.clazz = clazz;
+  }
+
+  public T[] sort(T[] arr) {
+    return mergeSort(arr, 0, arr.length - 1);
+  }
+
+  private T[] mergeSort(T[] arr, int start, int end) {
+    if (start == end) {
+      return Arrays.copyOfRange(arr, start, end + 1);
+    }
+
+    int middle = (start + end) / 2;
+    T[] result = merge(mergeSort(arr, start, middle), mergeSort(arr, middle + 1, end));
+
+    return result;
+  }
+
+  @SuppressWarnings("unchecked")
+  private T[] merge(T[] left, T[] right) {
+    int n = left.length;
+    int m = right.length;
+    T[] result = (T[]) Array.newInstance(clazz, m + n);
+    int i = 0, j = 0, k = 0;
+
+    while (i < n && j < m) {
+      int compare = compare(left[i], right[j]);
+      if (compare < 0) {
+        result[k] = left[i];
+        i++;
+      } else {
+        result[k] = right[j];
+        j++;
+      }
+      k++;
+    }
+
+    while (i < n) {
+      result[k] = left[i];
+      i++;
+      k++;
+    }
+
+    while (j < m) {
+      result[k] = right[j];
+      j++;
+      k++;
+    }
+
+    return result;
+  }
+
+  private int compare(T a, T b) {
+    if (a instanceof Integer && b instanceof Integer) {
+      return ((Integer) a).compareTo((Integer) b);
+    }
+    return -1;
+  }
+
+}
---
+++ algorithms/src/algorithms/util/InputUtil.java
@@ -0,0 +1,29 @@
+package algorithms.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class InputUtil {
+  private static final String INPUT = "input_files/";
+
+  public static String[] readContents(String fileName) throws FileNotFoundException {
+    StringBuffer buffer = new StringBuffer();
+    StringBuilder fileBuilder = new StringBuilder(INPUT);
+
+    File inputFile = new File(fileBuilder.append(fileName).toString());
+    FileReader fileReader = new FileReader(inputFile);
+    Scanner scanner = new Scanner(fileReader);
+
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine().trim();
+      if (line != null && !line.isEmpty()) {
+        buffer.append(line).append("\n");
+      }
+    }
+    scanner.close();
+
+    return buffer.toString().split("\n");
+  }
+}
