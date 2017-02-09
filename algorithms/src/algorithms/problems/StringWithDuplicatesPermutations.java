f1ce76316dee517afceb6dd1e416675e29ded52e
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-02-09 00:44:30 -0500

    Refactored using TreeMap

--- algorithms/src/algorithms/problems/StringWithDuplicatesPermutations.java
+++ algorithms/src/algorithms/problems/StringWithDuplicatesPermutations.java
@@ -4,9 +4,10 @@
 package algorithms.problems;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 
 import algorithms.util.InputUtil;
 
@@ -15,66 +16,34 @@ import algorithms.util.InputUtil;
  */
 public class StringWithDuplicatesPermutations {
 
-  class Character {
-    private char c;
-    private int count = 0;
-
-    public Character(Character obj) {
-      this.c = obj.c;
-      this.count = obj.count;
-    }
-
-    public Character(char c, int count) {
-      this.c = c;
-      this.count = count;
-    }
-
-    @Override
-    public String toString() {
-      return this.c + " " + this.count;
-    }
-  }
-
   public void generatePermutations(char[] arr) {
-    Arrays.sort(arr);
-    List<Character> charactersList = new ArrayList<Character>();
-    int i = 0, j = 0;
-    while (i < arr.length - 1) {
-      if (arr[i] != arr[i + 1]) {
-        charactersList.add(new Character(arr[i], j + 1));
-        j = 0;
-      } else {
-        j++;
-      }
-      i++;
+    Map<Character, Integer> counterMap = new TreeMap<Character, Integer>();
+    for (char c : arr) {
+      counterMap.put(Character.valueOf(c), counterMap.getOrDefault(c, 0) + 1);
     }
-    charactersList.add(new Character(arr[i], j + 1));
-    permutate(charactersList.toArray(new Character[charactersList.size()]), new StringBuilder());
+
+    permutate(arr, counterMap, new StringBuilder());
   }
 
-  private void permutate(Character[] characters, StringBuilder builder) {
+  private void permutate(char[] arr, Map<Character, Integer> counterMap, StringBuilder builder) {
     int counterSum = 0;
-    for (Character character : characters) {
-      if (character.count > 0) {
-        character.count--;
+    Set<Entry<Character, Integer>> entrySet = counterMap.entrySet();
+    for (Entry<Character, Integer> entry : entrySet) {
+      char c = entry.getKey();
+      int count = entry.getValue();
+      if (count > 0) {
         counterSum++;
-        builder.append(character.c);
-        permutate(characters, builder);
+        counterMap.put(c, count - 1);
+        builder.append(c);
+        permutate(arr, counterMap, builder);
         builder.setLength(builder.length() - 1);
-        character.count++;
+        counterMap.put(c, count);
       }
     }
 
     if (counterSum == 0) {
       System.out.println(builder.toString());
     }
-  }
-
-  private void print(Character[] characters) {
-    for (Character character : characters) {
-      System.out.print(character + " ");
-    }
-    System.out.println();
   }
 
   public static void main(String[] args) throws FileNotFoundException {
