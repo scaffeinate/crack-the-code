67c0fa5fba0503ab2262af857182cc713286685c
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-02-11 15:08:12 -0500

    Add solution to checkSubTree using PreOrder traversal

---
+++ cracking-the-coding-interview/src/chapter_4/CheckSubTree.java
@@ -0,0 +1,121 @@
+/**
+ * 
+ */
+package chapter_4;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import datastructures.tree.BinaryTree;
+import datastructures.tree.TreeNode;
+import util.InputUtil;
+
+/**
+ * @author Sudharsanan Muralidharan
+ */
+public class CheckSubTree {
+  BinaryTree<Integer> tree = null;
+  BinaryTree<Integer> subTree = null;
+  BinaryTree<Integer> currentTree = null;
+  Map<Integer, TreeNode<Integer>> treeNodes = null;
+
+  public CheckSubTree() {
+    tree = new BinaryTree<Integer>();
+    subTree = new BinaryTree<Integer>();
+    treeNodes = new HashMap<Integer, TreeNode<Integer>>();
+  }
+
+  private void constructTrees(String[] input) {
+    for (String line : input) {
+      String[] values = line.split(" ");
+      String action = values[0];
+      switch (action) {
+      case "tree":
+        currentTree = tree;
+        treeNodes.clear();
+        break;
+      case "subTree":
+        currentTree = subTree;
+        treeNodes.clear();
+        break;
+      case "insertRoot":
+        TreeNode<Integer> root = currentTree.insertRoot(Integer.parseInt(values[1]));
+        treeNodes.put(root.data, root);
+        break;
+      case "insertLeft":
+        insertChild(values, true);
+        break;
+      case "insertRight":
+        insertChild(values, false);
+        break;
+      case "print":
+        System.out.println(currentTree.breadthFirstTraversal(currentTree.getRoot()) + "\n");
+        break;
+      case "checkSubTree":
+        System.out.println(checkSubTree(tree, subTree));
+        break;
+      }
+    }
+  }
+
+  private void insertChild(String[] values, boolean isLeft) {
+    String[] nodes = values[1].split("->");
+    Integer parentVal = Integer.parseInt(nodes[0]);
+    Integer nodeVal = Integer.parseInt(nodes[1]);
+    TreeNode<Integer> parent = null;
+    if ((parent = treeNodes.get(parentVal)) != null) {
+      TreeNode<Integer> node = currentTree.insert(parent, new TreeNode<Integer>(nodeVal), isLeft);
+      treeNodes.put(nodeVal, node);
+    }
+  }
+
+  private boolean checkSubTree(BinaryTree<Integer> tree, BinaryTree<Integer> subTree) {
+    List<Integer> treeList = preOrder(tree.getRoot());
+    List<Integer> subTreeList = preOrder(subTree.getRoot());
+
+    int delta = treeList.size() - subTreeList.size();
+    int i = 0, j = 0;
+
+    while (i < delta) {
+      boolean matches = true;
+      while (j < subTreeList.size()) {
+        Integer treeListVal = treeList.get(i + j);
+        Integer subTreeListVal = subTreeList.get(j);
+        if (!treeListVal.equals(subTreeListVal)) {
+          i++;
+          matches = false;
+          break;
+        }
+        j++;
+      }
+
+      if (matches) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private List<Integer> preOrder(TreeNode<Integer> root) {
+    List<Integer> result = new ArrayList<Integer>();
+    if (root != null) {
+      result.add(root.data);
+      result.addAll(preOrder(root.left));
+      result.addAll(preOrder(root.right));
+    } else {
+      result.add(Integer.MIN_VALUE);
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) throws FileNotFoundException {
+    String[] input = InputUtil.readContents(4, "check_subtree");
+    CheckSubTree checkSubTree = new CheckSubTree();
+    checkSubTree.constructTrees(input);
+  }
+}
