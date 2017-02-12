e67e966e1bd1371050784dfd0d4b44521b842b7f
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-02-12 13:01:45 -0500

    Added solution for count inversions using Merge sort

--- algorithms/src/algorithms/problems/CountInversions.java
+++ algorithms/src/algorithms/problems/CountInversions.java
@@ -4,6 +4,7 @@
 package algorithms.problems;
 
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 import algorithms.util.InputUtil;
 
@@ -11,6 +12,11 @@ import algorithms.util.InputUtil;
  * @author Sudharsanan Muralidharan
  */
 public class CountInversions {
+
+  class InversionArray {
+    int[] arr;
+    int inversionCount = 0;
+  }
 
   private int countInversions(int[] arr) {
     int count = 0;
@@ -24,6 +30,67 @@ public class CountInversions {
     return count;
   }
 
+  private int countInversions2(int[] arr) {
+    return sortAndCountInverse(arr, 0, arr.length - 1).inversionCount;
+  }
+
+  private InversionArray sortAndCountInverse(int[] arr, int start, int end) {
+    if (start == end) {
+      InversionArray result = new InversionArray();
+      result.arr = Arrays.copyOfRange(arr, start, end + 1);
+      return result;
+    }
+
+    int middle = (start + end) / 2;
+    InversionArray leftInversions = sortAndCountInverse(arr, start, middle);
+    InversionArray rightInversions = sortAndCountInverse(arr, middle + 1, end);
+    InversionArray splitInversions = mergeAndCountSplitInversions(leftInversions, rightInversions);
+
+    InversionArray result = new InversionArray();
+    result.arr = splitInversions.arr;
+    result.inversionCount = leftInversions.inversionCount + rightInversions.inversionCount
+        + splitInversions.inversionCount;
+
+    return result;
+  }
+  
+  private InversionArray mergeAndCountSplitInversions(InversionArray leftInversions, InversionArray rightInversions) {
+    InversionArray inversionArray = new InversionArray();
+    int[] leftArr = leftInversions.arr;
+    int[] rightArr = rightInversions.arr;
+    int[] result = new int[leftArr.length + rightArr.length];
+    int i = 0, j = 0, k = 0, inversionCount = 0;
+
+    while (i < leftArr.length && j < rightArr.length) {
+      if (leftArr[i] <= rightArr[j]) {
+        result[k] = leftArr[i];
+        i++;
+      } else {
+        result[k] = rightArr[j];
+        j++;
+        inversionCount += (leftArr.length - i);
+      }
+      k++;
+    }
+
+    while (i < leftArr.length) {
+      result[k] = leftArr[i];
+      i++;
+      k++;
+    }
+
+    while (j < rightArr.length) {
+      result[k] = rightArr[j];
+      j++;
+      k++;
+    }
+
+    inversionArray.arr = result;
+    inversionArray.inversionCount = inversionCount;
+    
+    return inversionArray;
+  }
+
   public static void main(String[] args) throws FileNotFoundException {
     String[] input = InputUtil.readContents("test_count_inversions");
     CountInversions countInversions = new CountInversions();
@@ -33,7 +100,9 @@ public class CountInversions {
       for (int i = 0; i < values.length; i++) {
         arr[i] = Integer.parseInt(values[i]);
       }
-      System.out.println("Number of Inversions: " + countInversions.countInversions(arr));
+      System.out.println("Number of Inversions using brute force: " + countInversions.countInversions(arr));
+      System.out.println("Number of Inversions using divide and conquer: " + countInversions.countInversions2(arr));
+      System.out.println();
     }
   }
 }
