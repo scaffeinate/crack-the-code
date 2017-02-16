18a91b970d076dcb29386e3a84b6309da241be83
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-02-15 19:04:47 -0500

    Adding comments for heap, TODO: complete documenting BinaryHeap class

--- datastructures/src/datastructures/heap/BinaryHeap.java
+++ datastructures/src/datastructures/heap/BinaryHeap.java
@@ -2,35 +2,85 @@ package datastructures.heap;
 
 import java.lang.reflect.Array;
 
+/**
+ * Abstract class BinaryHeap
+ * 
+ * @param <T>
+ * 
+ * @author Sudharsanan Muralidharan
+ */
 public abstract class BinaryHeap<T> {
+  // Set Initial heapArray Capacity to 32
   private final int DEFAULT_INITIAL_CAPACITY = 32;
   private int heapCapacity;
 
   protected HeapType type;
   protected T[] heapArr;
+  /*
+   * Since the heapArr operations start from index = 1, index = 0 is left empty.
+   * So set initial size = 1.
+   */
   protected int size = 1;
 
+  /**
+   * Constructor which takes in clazz, initialCapacity of the heap and HeapType
+   * 
+   * @param clazz
+   * @param initialCapacity
+   * @param type
+   */
   @SuppressWarnings("unchecked")
   public BinaryHeap(Class<?> clazz, int initialCapacity, HeapType type) {
+    /*
+     * If initialCapacity is not specified then default to
+     * DEFAULT_INITIAL_CAPACITY
+     */
     heapCapacity = (initialCapacity == -1) ? DEFAULT_INITIAL_CAPACITY : initialCapacity;
     heapArr = (T[]) Array.newInstance(clazz, heapCapacity);
     this.type = type;
   }
 
+  /**
+   * Inserts new element into the heap
+   * 
+   * @param data
+   */
   public void insert(T data) {
+    /*
+     * If we reached the heapCapacity then growHeap()
+     */
     if (size == heapCapacity) {
       growHeap();
     }
 
+    /*
+     * Use size variable as the index where new element needs to be inserted.
+     * NOTE: size-1 indicates the actual size of the heap.
+     */
     heapArr[size] = data;
+    // Insert the root
     if (size == 1) {
       size++;
       return;
     }
 
-    int current = size;
-    int parent = (current / 2);
+    /*
+     * Bubble up the element that we inserted to the correct position so that
+     * the heap property is maintained.
+     */
+    int current = size; // start with the index where we inserted
+    int parent = (current / 2); // Compute the parent of the element inserted
+
+    /*
+     * Bubble up the element unless it stops Violating Heap Property and check
+     * till the root at index=1
+     */
     while (parent > 0 && violatesProperty(current, parent)) {
+      /*
+       * Swap current and parent if violation occurs. Make the parent as current
+       * and compute it's parent and repeat till violatesProperty is false and
+       * parent is root.
+       */
       swapElements(current, parent);
       current = parent;
       parent = (current / 2);
@@ -38,6 +88,17 @@ public abstract class BinaryHeap<T> {
     size++;
   }
 
+  /**
+   * Given an arr builds the heap using heapify(). Start from the middle of
+   * heapArr which indicates the last level of Non-Leaf nodes. While the index >
+   * 0 heapify().
+   * 
+   * Complexity: O(nlgn), Tighter Bound: O(n)
+   * 
+   * Ref: https://www.cs.bgu.ac.il/~ds122/wiki.files/Presentation09.pdf
+   * 
+   * @param arr
+   */
   @SuppressWarnings("unchecked")
   public void buildHeap(T[] arr) {
     size = arr.length + 1;
