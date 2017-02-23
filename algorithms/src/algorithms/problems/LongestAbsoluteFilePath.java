45fa52fc5e83298ef64ecc89e31203554ccde3b1
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-02-23 00:55:29 -0500

    Add solution to fetch the longest absolute file path

--- algorithms/src/algorithms/problems/LongestAbsoluteFilePath.java
+++ algorithms/src/algorithms/problems/LongestAbsoluteFilePath.java
@@ -18,37 +18,36 @@ public class LongestAbsoluteFilePath {
     String[] lines = input.split("\n");
     int longest = 0;
     StringBuilder builder = new StringBuilder();
-    stack.push(lines[0]);
-    builder.append(lines[0]);
-    
-    for (int i = 1; i < lines.length; i++) {
+    stack.push("/");
+    builder.append("/");
+
+    for (int i = 0; i < lines.length; i++) {
       String fileName = lines[i];
       while (level(fileName) <= level(stack.peek())) {
-        String top = trimTabs(stack.pop());
-        builder.setLength(builder.length() - top.length() - 1);
+        String top = stack.pop().trim();
+        builder.setLength(builder.length() - top.length());
       }
 
       stack.push(fileName);
-      fileName = trimTabs(fileName);
-      builder.append(fileName).append("/");
-      if (isFile(fileName) && builder.length() > longest) {
+      builder.append(fileName.trim());
+      if (isImgFile(fileName.trim()) && builder.length() > longest) {
         longest = builder.length();
       }
     }
 
     return longest;
   }
-  
-  private static String trimTabs(String s) {
-    return s.replaceAll("\t", "");
-}
 
-  private static boolean isFile(String fileName) {
-    return fileName.indexOf('.') != -1;//&& fileName.matches("([^\\s]+(\\.(?i)(jpg|png|gif|bmp))$)");
+  private static boolean isImgFile(String fileName) {
+    return fileName.indexOf('.') != -1 && fileName.matches("([^\\s]+(\\.(?i)(jpg|png|gif|bmp))$)");
   }
 
   private static int level(String fileName) {
-    return fileName.lastIndexOf('\t') + 1;
+    if(fileName.equals("/")) {
+      return -1;
+    }
+
+    return fileName.lastIndexOf(' ') + 1;
   }
 
   public static void main(String[] args) throws FileNotFoundException {
