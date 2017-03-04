50dd66073780b70b0abea04c8c86193cfb9d33b3
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-04 00:09:18 -0500

    Added test runners for all sorting implementations

---
+++ algorithms/test/algorithms/sort/TestInsertionSort.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package algorithms.sort;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import algorithms.util.InputUtil;
+
+/**
+ * @author Sudharsanan Muralidharan
+ */
+public class TestInsertionSort {
+  private static String basePath = "input_files/sort/";
+  private static InsertionSort<Integer> insertionSort = null;
+  private static String[] testCases = new String[] { "test_case_1", "test_case_2", "test_case_3" };;
+  private static List<String[]> inputList = new ArrayList<String[]>();
+
+  @BeforeClass
+  public static void setup() {
+    insertionSort = new InsertionSort<Integer>();
+    for (String testCase : testCases) {
+      String inputFile = basePath + testCase;
+      inputList.add(InputUtil.readContents(inputFile));
+    }
+  }
+
+  @AfterClass
+  public static void teardown() {
+    insertionSort = null;
+    testCases = null;
+    inputList = null;
+  }
+
+  @Test
+  public void testInsertionSortTestCase1() {
+    assertResult(inputList.get(0));
+  }
+  
+  @Test
+  public void testInsertionSortTestCase2() {
+    assertResult(inputList.get(1));
+  }
+  
+  @Test
+  public void testInsertionSortTestCase3() {
+    assertResult(inputList.get(2));
+  }
+  
+  private void assertResult(String[] input) {
+    String line = input[0];
+    String[] values = line.split(" ");
+    Integer[] arr = new Integer[values.length];
+    for (int i = 0; i < values.length; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    Integer[] expected = arr.clone();
+    insertionSort.sort(arr);
+    Arrays.sort(expected);
+
+    assertTrue(validateResult(arr, expected));
+  }
+
+  private boolean validateResult(Integer[] arr, Integer[] expected) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i].intValue() != expected[i].intValue()) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
---
+++ algorithms/test/algorithms/sort/TestSelectionSort.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package algorithms.sort;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import algorithms.util.InputUtil;
+
+/**
+ * @author Sudharsanan Muralidharan
+ */
+public class TestSelectionSort {
+  private static String basePath = "input_files/sort/";
+  private static SelectionSort<Integer> selectionSort = null;
+  private static String[] testCases = new String[] { "test_case_1", "test_case_2", "test_case_3" };;
+  private static List<String[]> inputList = new ArrayList<String[]>();
+
+  @BeforeClass
+  public static void setup() {
+    selectionSort = new SelectionSort<Integer>();
+    for (String testCase : testCases) {
+      String inputFile = basePath + testCase;
+      inputList.add(InputUtil.readContents(inputFile));
+    }
+  }
+
+  @AfterClass
+  public static void teardown() {
+    selectionSort = null;
+    testCases = null;
+    inputList = null;
+  }
+
+  @Test
+  public void testSelectionSortTestCase1() {
+    assertResult(inputList.get(0));
+  }
+  
+  @Test
+  public void testSelectionSortTestCase2() {
+    assertResult(inputList.get(1));
+  }
+  
+  @Test
+  public void testSelectionSortTestCase3() {
+    assertResult(inputList.get(2));
+  }
+  
+  private void assertResult(String[] input) {
+    String line = input[0];
+    String[] values = line.split(" ");
+    Integer[] arr = new Integer[values.length];
+    for (int i = 0; i < values.length; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    Integer[] expected = arr.clone();
+    selectionSort.sort(arr);
+    Arrays.sort(expected);
+
+    assertTrue(validateResult(arr, expected));
+  }
+
+  private boolean validateResult(Integer[] arr, Integer[] expected) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i].intValue() != expected[i].intValue()) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
