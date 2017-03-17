ade9160a54f9b79a4914adcc95cf1734be72bcde
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-16 23:00:08 -0400

    Crazy Fast Console Output using BufferedWriter :clap:

--- hackerrank/src/week_of_code_30/MelodiousPassword.java
+++ hackerrank/src/week_of_code_30/MelodiousPassword.java
@@ -1,50 +1,55 @@
 package week_of_code_30;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class MelodiousPassword {
 
-  private static char[] consonants = new char[] { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r',
-      's', 't', 'v', 'w', 'x', 'z' };
-  private static char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+  private char[] consonants = null;
+  private char[] vowels = null;
+  private BufferedWriter log = null;
 
-  private static void generate(int n) {
-    List<String> results = new ArrayList<String>();
-    if (n == 1 || (n % 2 == 0)) {
-      generate(0, n, new StringBuilder(), true, results);
-      generate(0, n, new StringBuilder(), false, results);
-    } else {
-      generate(0, n, new StringBuilder(), false, results);
-    }
+  public MelodiousPassword() {
+    log = new BufferedWriter(new OutputStreamWriter(System.out));
+    consonants = new char[] { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w',
+        'x', 'z' };
+    vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
   }
 
-  private static void generate(int index, int n, StringBuilder builder, boolean isVowel, List<String> results) {
+  private void generate(int n) throws IOException {
+    generate(0, n, new StringBuilder(), true);
+    generate(0, n, new StringBuilder(), false);
+    log.flush();
+  }
+
+  private void generate(int index, int n, StringBuilder builder, boolean isVowel) throws IOException {
     if (index == n) {
-      System.out.println(builder.toString());
+      log.append(builder.toString()).append("\n");
       return;
     }
 
     if (isVowel) {
       for (int i = 0; i < vowels.length; i++) {
         builder.append(vowels[i]);
-        generate(index + 1, n, builder, !isVowel, results);
+        generate(index + 1, n, builder, !isVowel);
         builder.setLength(builder.length() - 1);
       }
     } else {
       for (int i = 0; i < consonants.length; i++) {
         builder.append(consonants[i]);
-        generate(index + 1, n, builder, !isVowel, results);
+        generate(index + 1, n, builder, !isVowel);
         builder.setLength(builder.length() - 1);
       }
     }
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    generate(n);
+    MelodiousPassword melodiousPassword = new MelodiousPassword();
+    melodiousPassword.generate(n);
     in.close();
   }
 }
