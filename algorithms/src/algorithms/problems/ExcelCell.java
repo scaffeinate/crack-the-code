99877c8b8b1646bce491e1b1ab2932990481f2c7
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-17 21:08:12 -0400

    Need to cleanup logic and revisit Excel Cell

--- algorithms/src/algorithms/problems/ExcelCell.java
+++ algorithms/src/algorithms/problems/ExcelCell.java
@@ -1,14 +1,24 @@
 package algorithms.problems;
 
-import java.util.Scanner;
-
 public class ExcelCell {
 
-  public static String computeCell(int n) {
-    return computeCell(n, (n % 26) == 0);
+  public int getColumn(String s) {
+    int sum = 0;
+
+    for (int i = s.length() - 1, j = 0; i >= 0; i--, j++) {
+      char c = s.charAt(i);
+      int val = c - 'A' + 1;
+      sum += (val * Math.pow(26, j));
+    }
+
+    return sum;
   }
 
-  public static String computeCell(int n, boolean modZero) {
+  public String getCell(int n) {
+    return getCell(n, (n % 26) == 0);
+  }
+
+  private String getCell(int n, boolean modZero) {
     StringBuilder builder = new StringBuilder();
     if (n < 26) {
       return String.valueOf((Character.valueOf((char) (n + 'A' - (modZero ? 2 : 1)))));
@@ -16,18 +26,11 @@ public class ExcelCell {
       if (n / 26 == 1) {
         return "Z";
       } else {
-        builder.append(computeCell((n / 26), (n % 26) == 0))
+        builder.append(getCell((n / 26), (n % 26) == 0))
             .append((n % 26) == 0 ? 'Z' : Character.valueOf((char) ((n % 26) + 'A' - 1)));
       }
     }
 
     return builder.toString();
-  }
-
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    int n = in.nextInt();
-    System.out.println(computeCell(n));
-    in.close();
   }
 }
