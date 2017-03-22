479c7c1a8910ebd1d2a0b05da1512b39e96f9df6
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-21 23:47:54 -0400

    Add test case 1 and Test runner

---
+++ algorithms/test/algorithms/graph/TestFloydWarshallShortestPath.java
@@ -0,0 +1,73 @@
+package algorithms.graph;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import algorithms.util.InputUtil;
+
+public class TestFloydWarshallShortestPath {
+  private static final String basePath = "input_files/graph/all_pairs_shortest_path/";
+  private static String[] testCases = new String[] { "test_case_1", "test_case_2", "test_case_3", "test_case_4" };
+  private static List<String[]> inputList = new ArrayList<String[]>();
+  private FloydWarshallShortestPath floydWarshall = null;
+
+  @BeforeClass
+  public static void setup() {
+    for (String testCase : testCases) {
+      String inputFile = basePath + testCase;
+      inputList.add(InputUtil.readContents(inputFile));
+    }
+  }
+
+  @AfterClass
+  public static void teardown() {
+    testCases = null;
+    inputList = null;
+  }
+
+  @Before
+  public void setupTest() {
+    floydWarshall = new FloydWarshallShortestPath();
+  }
+
+  @After
+  public void tearDownTest() {
+    floydWarshall = null;
+  }
+
+  @Test
+  public void testFloydWarshallShortestPathTestCase1() {
+    String[] input = inputList.get(0);
+    floydWarshall.constructGraph(input);
+    floydWarshall.computeShortestPath();
+    assertShortestPath(input);
+  }
+  
+  private void assertShortestPath(String[] input) {
+    for (String line : input) {
+      String[] values = line.split(" ");
+      switch (values[0]) {
+      case "shortestDistance":
+        Integer expectedDistance = null;
+        if (values[3].equals("infinity")) {
+          expectedDistance = Integer.MAX_VALUE;
+        } else if (values[3].equals("-infinity")) {
+          expectedDistance = Integer.MIN_VALUE;
+        } else {
+          expectedDistance = Integer.parseInt(values[3]);
+        }
+        assertThat(floydWarshall.getShortestDistanceTo(values[1], values[2]), is(expectedDistance));
+        break;
+      }
+    }
+  }
+}
