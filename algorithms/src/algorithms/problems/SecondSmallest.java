136053b1f19625d2e94653addd138c20598a20dd
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-23 00:48:12 -0400

    Add test cases for second smallest

---
+++ algorithms/src/algorithms/problems/SecondSmallest.java
@@ -0,0 +1,25 @@
+package algorithms.problems;
+
+public class SecondSmallest {
+  public int fetchSecondSmallest(int[] arr) {
+    if (arr.length < 2) {
+      return -1;
+    }
+
+    int smallest, smallest2;
+
+    smallest = Math.min(arr[0], arr[1]);
+    smallest2 = Math.max(arr[0], arr[1]);
+
+    for (int i = 2; i < arr.length; i++) {
+      if (arr[i] < smallest) {
+        smallest2 = smallest;
+        smallest = arr[i];
+      } else if (arr[i] < smallest2) {
+        smallest2 = arr[i];
+      }
+    }
+
+    return smallest2;
+  }
+}
--- algorithms/test/algoritms/problems/TestSecondSmallest.java
+++ algorithms/test/algoritms/problems/TestSecondSmallest.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package algoritms.problems;
 
 import static org.junit.Assert.assertEquals;
@@ -12,21 +9,18 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import algorithms.problems.CountInversions;
+import algorithms.problems.SecondSmallest;
 import algorithms.util.InputUtil;
 
-/**
- * @author Sudharsanan Muralidharan
- */
-public class TestCountInversions {
-  private static CountInversions countInversions = null;
-  private static final String basePath = "input_files/problems/count_inversions/";
+public class TestSecondSmallest {
+  private static final String basePath = "input_files/problems/second_smallest/";
   private static String[] testCases = new String[] { "test_case_1", "test_case_2", "test_case_3", "test_case_4" };
   private static List<String[]> inputList = new ArrayList<String[]>();
+  private static SecondSmallest secondSmallest = null;
 
   @BeforeClass
   public static void setup() {
-    countInversions = new CountInversions();
+    secondSmallest = new SecondSmallest();
     for (String testCase : testCases) {
       String inputFile = basePath + testCase;
       inputList.add(InputUtil.readContents(inputFile));
@@ -35,52 +29,52 @@ public class TestCountInversions {
 
   @AfterClass
   public static void teardown() {
-    countInversions = null;
+    secondSmallest = null;
     testCases = null;
     inputList = null;
   }
 
   @Test
-  public void testCountInversionsTestCase1() {
+  public void testSecondSmallestTestCase1() {
     String[] input = inputList.get(0);
     String[] values = input[0].split(" ");
     int[] arr = new int[values.length];
     for (int i = 0; i < values.length; i++) {
       arr[i] = Integer.parseInt(values[i]);
     }
-    assertEquals(countInversions.countInversions(arr), 36);
+    assertEquals(secondSmallest.fetchSecondSmallest(arr), 2);
   }
 
   @Test
-  public void testCountInversionsTestCase2() {
+  public void testSecondSmallestTestCase2() {
     String[] input = inputList.get(1);
     String[] values = input[0].split(" ");
     int[] arr = new int[values.length];
     for (int i = 0; i < values.length; i++) {
       arr[i] = Integer.parseInt(values[i]);
     }
-    assertEquals(countInversions.countInversions(arr), 0);
+    assertEquals(secondSmallest.fetchSecondSmallest(arr), 2);
   }
 
   @Test
-  public void testCountInversionsTestCase3() {
+  public void testSecondSmallestTestCase3() {
     String[] input = inputList.get(2);
     String[] values = input[0].split(" ");
     int[] arr = new int[values.length];
     for (int i = 0; i < values.length; i++) {
       arr[i] = Integer.parseInt(values[i]);
     }
-    assertEquals(countInversions.countInversions(arr), 0);
+    assertEquals(secondSmallest.fetchSecondSmallest(arr), 2);
   }
 
   @Test
-  public void testCountInversionsTestCase4() {
+  public void testSecondSmallestTestCase4() {
     String[] input = inputList.get(3);
     String[] values = input[0].split(" ");
     int[] arr = new int[values.length];
     for (int i = 0; i < values.length; i++) {
       arr[i] = Integer.parseInt(values[i]);
     }
-    assertEquals(countInversions.countInversions(arr), 15);
+    assertEquals(secondSmallest.fetchSecondSmallest(arr), 2);
   }
 }
