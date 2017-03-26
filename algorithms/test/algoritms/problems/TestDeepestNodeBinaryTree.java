a209ee7d6dade8a339687e4c3a480e7040695548
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-25 23:59:15 -0400

    Added 2 more test cases

--- algorithms/test/algoritms/problems/TestDeepestNodeBinaryTree.java
+++ algorithms/test/algoritms/problems/TestDeepestNodeBinaryTree.java
@@ -14,7 +14,7 @@ import algorithms.util.InputUtil;
 
 public class TestDeepestNodeBinaryTree {
   private static final String basePath = "input_files/tree/deepest_node_binary_tree/";
-  private static String[] testCases = new String[] { "test_case_1", "test_case_2" };
+  private static String[] testCases = new String[] { "test_case_1", "test_case_2", "test_case_3", "test_case_4" };
   private static List<String[]> inputList = new ArrayList<String[]>();
   private static DeepestNodeBinaryTree deepestNode = null;
 
@@ -38,6 +38,7 @@ public class TestDeepestNodeBinaryTree {
     String[] input = inputList.get(0);
     deepestNode.constructTree(input);
     assertEquals(Integer.valueOf(12), deepestNode.getDeepestNode());
+    assertEquals(Integer.valueOf(12), deepestNode.getDeepestNode2());
   }
   
   @Test
@@ -45,5 +46,22 @@ public class TestDeepestNodeBinaryTree {
     String[] input = inputList.get(1);
     deepestNode.constructTree(input);
     assertEquals(Integer.valueOf(9), deepestNode.getDeepestNode());
+    assertEquals(Integer.valueOf(9), deepestNode.getDeepestNode2());
+  }
+  
+  @Test
+  public void testDeepestNodeBinaryTreeTestCase3() {
+    String[] input = inputList.get(2);
+    deepestNode.constructTree(input);
+    assertEquals(Integer.valueOf(5), deepestNode.getDeepestNode());
+    assertEquals(Integer.valueOf(5), deepestNode.getDeepestNode2());
+  }
+  
+  @Test
+  public void testDeepestNodeBinaryTreeTestCase4() {
+    String[] input = inputList.get(3);
+    deepestNode.constructTree(input);
+    assertEquals(Integer.valueOf(15), deepestNode.getDeepestNode());
+    assertEquals(Integer.valueOf(15), deepestNode.getDeepestNode2());
   }
 }
