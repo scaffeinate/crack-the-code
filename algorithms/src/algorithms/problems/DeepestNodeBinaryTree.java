53c573162574cbd524d77bd5c356d66e4f82c3aa
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-25 23:59:07 -0400

    Added alternate Topdown implementation for deepestNode

--- algorithms/src/algorithms/problems/DeepestNodeBinaryTree.java
+++ algorithms/src/algorithms/problems/DeepestNodeBinaryTree.java
@@ -38,16 +38,22 @@ public class DeepestNodeBinaryTree {
   public Integer getDeepestNode() {
     return deepestNode(tree.getRoot()).treeNode.data;
   }
-  
+
+  public Integer getDeepestNode2() {
+    TreeNodeWrapper<Integer> deepestWrapper = new TreeNodeWrapper<>(tree.getRoot(), 0);
+    deepestNode(tree.getRoot(), deepestWrapper, 0);
+    return deepestWrapper.treeNode.data;
+  }
+
   private TreeNodeWrapper<Integer> deepestNode(TreeNode<Integer> root) {
-    if(root == null) {
+    if (root == null) {
       return new TreeNodeWrapper<Integer>(null, -1);
     }
-    
+
     TreeNodeWrapper<Integer> leftWrapper = deepestNode(root.left);
     TreeNodeWrapper<Integer> rightWrapper = deepestNode(root.right);
-    
-    if(leftWrapper.treeNode == null && rightWrapper.treeNode == null) {
+
+    if (leftWrapper.treeNode == null && rightWrapper.treeNode == null) {
       return new TreeNodeWrapper<Integer>(root, 1);
     } else {
       TreeNodeWrapper<Integer> deepestWrapper = (leftWrapper.depth >= rightWrapper.depth) ? leftWrapper : rightWrapper;
@@ -55,24 +61,39 @@ public class DeepestNodeBinaryTree {
       return deepestWrapper;
     }
   }
-  
+
+  private void deepestNode(TreeNode<Integer> root, TreeNodeWrapper<Integer> deepestWrapper,
+      int depth) {
+    if (root == null) {
+      return;
+    }
+
+    deepestNode(root.left, deepestWrapper, depth + 1);
+    deepestNode(root.right, deepestWrapper, depth + 1);
+
+    if(deepestWrapper.depth < depth) {
+      deepestWrapper.depth = depth;
+      deepestWrapper.treeNode = root;
+    }
+  }
+
   private void insert(String parentStr, String nodeStr, boolean isLeft) {
     int parentVal = Integer.parseInt(parentStr);
     int nodeVal = Integer.parseInt(nodeStr);
     TreeNode<Integer> parentNode = nodesMap.get(parentVal);
-    if(parentNode == null) {
+    if (parentNode == null) {
       return;
     }
-    
+
     TreeNode<Integer> node = new TreeNode<Integer>(nodeVal);
     nodesMap.put(nodeVal, tree.insert(parentNode, node, isLeft));
   }
-  
+
   @SuppressWarnings("hiding")
   private class TreeNodeWrapper<Integer> {
     TreeNode<Integer> treeNode = null;
     int depth = 0;
-    
+
     public TreeNodeWrapper(TreeNode<Integer> treeNode, int depth) {
       this.treeNode = treeNode;
       this.depth = depth;
