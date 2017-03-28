7c67b306702cf5b0bcfb34798256e565f73fb26f
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-28 00:12:09 -0400

    Using -1 instead of ~0 for Insertion

--- cracking-the-coding-interview/src/chapter_5/Insertion.java
+++ cracking-the-coding-interview/src/chapter_5/Insertion.java
@@ -7,8 +7,8 @@ public class Insertion {
   }
 
   private int clearBits(int n, int i, int j) {
-    int leftCleared = ~(0) >>> (31 - j); // 00111111
-    int rightCleared = ~(0) << i; // 11111100
+    int leftCleared = -1 >>> (31 - j); // 00111111
+    int rightCleared = -1 << i; // 11111100
     int mask = ~(leftCleared & rightCleared); // mask 00111100, ~(mask) 11000011
     return n & mask;
   }
