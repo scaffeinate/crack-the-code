fad1a2ab8dc67f62e9b20342d9da967463e9c8ab
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-29 00:09:24 -0400

    Cleanup logic

--- cracking-the-coding-interview/src/chapter_5/FlipBitToWin.java
+++ cracking-the-coding-interview/src/chapter_5/FlipBitToWin.java
@@ -5,17 +5,17 @@ public class FlipBitToWin {
   public int flipBitToWin(String binaryString) {
     int max = Integer.MIN_VALUE;
     int val = Integer.parseInt(binaryString, 2);
-    int leftCounter = 0, rightCounter = 0, zeroVal = 0;
+    int leftCounter = 0, rightCounter = 0, zeroCounter = 0;
     while (val > 0) {
       if ((val & (1 << 0)) != 0) {
         leftCounter++;
       } else {
         rightCounter = leftCounter;
         leftCounter = 0;
-        zeroVal = 1;
+        zeroCounter = 1;
       }
       
-      max = Math.max(max, (leftCounter + rightCounter + zeroVal));      
+      max = Math.max(max, (leftCounter + rightCounter + zeroCounter));
       val >>= 1;
     }
 
