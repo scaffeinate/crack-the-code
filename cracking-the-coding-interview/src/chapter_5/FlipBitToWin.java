73ad320677c2b3d7238ec9734a2927522fc90076
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-28 23:59:37 -0400

    Simpler logic that calculates max at each step

--- cracking-the-coding-interview/src/chapter_5/FlipBitToWin.java
+++ cracking-the-coding-interview/src/chapter_5/FlipBitToWin.java
@@ -6,22 +6,20 @@ public class FlipBitToWin {
     int max = Integer.MIN_VALUE;
     int val = Integer.parseInt(binaryString, 2);
     int leftCounter = 0, rightCounter = 0, zeroVal = 0;
-    boolean firstZero = true;
     while (val > 0) {
       if ((val & (1 << 0)) != 0) {
         leftCounter++;
       } else {
-        max = firstZero ? max : Math.max(max, (leftCounter + rightCounter + 1));
-        firstZero = false;
         rightCounter = leftCounter;
         leftCounter = 0;
         zeroVal = 1;
       }
-
+      
+      max = Math.max(max, (leftCounter + rightCounter + zeroVal));      
       val >>= 1;
     }
 
-    return Math.max(max, (leftCounter + rightCounter + zeroVal));
+    return max;
   }
 
 }
