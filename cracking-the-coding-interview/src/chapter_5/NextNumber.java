82c2da1a0ecee6dd596ed65b3314c0997d3e27d1
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-03-30 00:59:52 -0400

    Added the optimized solution using bit shift

--- cracking-the-coding-interview/src/chapter_5/NextNumber.java
+++ cracking-the-coding-interview/src/chapter_5/NextNumber.java
@@ -3,6 +3,10 @@ package chapter_5;
 public class NextNumber {
 
   public int getNext(int n) {
+    if (n == 0) {
+      return 0;
+    }
+
     int onesCount = countOnes(n);
     for (int i = n + 1; i <= Integer.MAX_VALUE; i++) {
       if (countOnes(i) == onesCount) {
@@ -13,6 +17,10 @@ public class NextNumber {
   }
 
   public int getPrevious(int n) {
+    if (n == 0) {
+      return 0;
+    }
+
     int onesCount = countOnes(n);
     for (int i = n - 1; i >= 0; i--) {
       if (countOnes(i) == onesCount) {
@@ -22,12 +30,68 @@ public class NextNumber {
     return n;
   }
 
+  public int getNext2(int n) {
+    int numOnes = 0, p = 0;
+    for (int i = 0; i < 32; i++) {
+      if (isBitSet(n, i)) {
+        numOnes++;
+      } else if (numOnes > 0) {
+        p = i;
+        n = setBit(n, p);
+        break;
+      }
+    }
+
+    int mask = ~((1 << p) - 1);
+    n = n & mask;
+
+    int rightOnesMask = (1 << (numOnes - 1)) - 1;
+
+    return n | rightOnesMask;
+  }
+
+  public int getPrevious2(int n) {
+    int numZeros = 0, p = 0;
+    for (int i = 0; i < 32; i++) {
+      if(isBitSet(n ,i)) {
+        if(numZeros > 0) {
+          p = i;
+          n = unsetBit(n, i);
+          break;
+        }
+      } else {
+        numZeros++;
+      }
+    }
+    
+    int mask = ~((1 << p) - 1);
+    n = n & mask;
+    
+    int rightOnesMask = ((1 << (p)) - 1);
+    int zerosMask = (~0) << (numZeros - 1);
+    rightOnesMask &= zerosMask;
+    
+    return n | rightOnesMask;
+  }
+
   private int countOnes(int n) {
     int numOnes = 0;
     for (int i = 0; i < 32; i++) {
-      numOnes += ((n & (1 << i)) == 0) ? 0 : 1;
+      numOnes += (isBitSet(n, i)) ? 1 : 0;
     }
 
     return numOnes;
+  }
+
+  private int setBit(int n, int i) {
+    return n | (1 << i);
+  }
+  
+  private int unsetBit(int n, int i) {
+    return n & (~(1 << i));
+  }
+
+  private boolean isBitSet(int n, int i) {
+    return (n & (1 << i)) != 0;
   }
 }
