3fe93a89a2b9f6dd32c5187563561f50e48d1459
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-02 23:07:06 -0400

    Convert leetcode to gradle project

--- leetcode/src/leetcode/week_of_code_26/FriendCircles.java
+++ leetcode/src/leetcode/week_of_code_26/FriendCircles.java
@@ -10,7 +10,7 @@ public class FriendCircles {
     DisjointSetWithPathCompression<Integer> disjointSets = new DisjointSetWithPathCompression<Integer>();
 
     for (int i = 0; i < m.length; i++) {
-      if(m[i][i] == 1) {
+      if (m[i][i] == 1) {
         disjointSets.makeSet(i);
       }
     }
@@ -22,15 +22,13 @@ public class FriendCircles {
         }
       }
     }
-    
+
     return disjointSets.getDisjointSets().size();
   }
 
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    int[][] m = new int[][] {new int[]{1,1,0},
-                 new int[]{1,1,1},
-                 new int[]{0,1,1}};
+    int[][] m = new int[][] { new int[] { 1, 1, 0 }, new int[] { 1, 1, 1 }, new int[] { 0, 1, 1 } };
     FriendCircles friendCircles = new FriendCircles();
     System.out.println(friendCircles.findCircleNum(m));
     in.close();
