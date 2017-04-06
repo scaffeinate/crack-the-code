e9de850cc93305b88e5aa8a596e9314f56faf0a9
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-06 00:12:48 -0400

    Added solution for is frequency less than digit

---
+++ geeks-for-geeks/src/random/IsDigitFrequencyLessThanDigit.java
@@ -0,0 +1,23 @@
+package random;
+
+public class IsDigitFrequencyLessThanDigit {
+
+  public boolean isFrequencyLessThanDigit(int n) {
+    if(n == 0) {
+      return false;
+    }
+    
+    int digit = 0;
+    int[] arr = new int[10];
+    while (n > 0) {
+      digit = (n % 10);
+      n /= 10;
+      arr[digit]++;
+      if (arr[digit] > digit) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
