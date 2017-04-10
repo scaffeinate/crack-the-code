062c1575f5eb6de9cee8f326002cfdcaa735e310
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-09 23:40:41 -0400

    Add question link :P

--- geeks-for-geeks/src/binarytree/CheckBSTHasDeadEnd.java
+++ geeks-for-geeks/src/binarytree/CheckBSTHasDeadEnd.java
@@ -3,6 +3,12 @@ package binarytree;
 import datastructures.tree.BinarySearchTree;
 import datastructures.tree.TreeNode;
 
+/**
+ * Question:
+ * http://www.geeksforgeeks.org/check-whether-bst-contains-dead-end-not/
+ * 
+ * @author Sudharsanan Muralidharan
+ */
 public class CheckBSTHasDeadEnd {
   private BinarySearchTree<Integer> tree = null;
   private TreeNode<Integer> root = null;
@@ -33,12 +39,13 @@ public class CheckBSTHasDeadEnd {
   }
 
   private boolean hasDeadEnd(TreeNode<Integer> root, Range range) {
-    if(root == null) {
+    if (root == null) {
       return false;
     } else if (root.left == null && root.right == null) {
       return Math.abs(root.data - range.min) <= 1 && Math.abs(range.max - root.data) <= 1;
     } else {
-      return hasDeadEnd(root.left, new Range(range.min, root.data)) || hasDeadEnd(root.right, new Range(root.data, range.max));
+      return hasDeadEnd(root.left, new Range(range.min, root.data))
+          || hasDeadEnd(root.right, new Range(root.data, range.max));
     }
   }
 
