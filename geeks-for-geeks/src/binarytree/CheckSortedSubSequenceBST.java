222cc693f5ad44e3bdff439147041fd436ae01ec
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-12 23:30:05 -0400

    Added solution for checking sorted subsequence in a BST

---
+++ geeks-for-geeks/src/binarytree/CheckSortedSubSequenceBST.java
@@ -0,0 +1,45 @@
+package binarytree;
+
+import datastructures.tree.BinarySearchTree;
+import datastructures.tree.TreeNode;
+
+public class CheckSortedSubSequenceBST {
+  private BinarySearchTree<Integer> tree = null;
+  private TreeNode<Integer> root = null;
+
+  public void constructTree(String[] input) {
+    tree = new BinarySearchTree<Integer>();
+    for (String line : input) {
+      String[] values = line.split(" ");
+      String action = values[0];
+
+      switch (action) {
+      case "insertRoot":
+        tree = new BinarySearchTree<Integer>();
+        root = tree.insertRoot(Integer.parseInt(values[1]));
+        break;
+      case "insert":
+        tree.insert(root, Integer.parseInt(values[1]));
+        break;
+      }
+    }
+  }
+
+  public boolean checkIfSeqExists(int[] sequence) {
+    int index = inOrder(root, sequence, 0);
+    return (index == sequence.length);
+  }
+
+  private int inOrder(TreeNode<Integer> root, int[] sequence, int index) {
+    if (root == null) {
+      return index;
+    }
+
+    index = inOrder(root.left, sequence, index);
+    if (root.data == sequence[index]) {
+      index += 1;
+    }
+    index = inOrder(root.right, sequence, index);
+    return index;
+  }
+}
