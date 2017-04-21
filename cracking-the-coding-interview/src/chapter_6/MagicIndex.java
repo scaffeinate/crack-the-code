7f0208c42923dd4a7b3178f5e730ec480e9f9ca2
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-21 02:29:16 -0400

    Changed to book solution since it’s much cleaner

--- cracking-the-coding-interview/src/chapter_6/MagicIndex.java
+++ cracking-the-coding-interview/src/chapter_6/MagicIndex.java
@@ -2,19 +2,19 @@ package chapter_6;
 
 public class MagicIndex {
   public int findMagicIndex(int[] arr) {
-    return findMagicIndex(arr, 0, arr.length);
+    return findMagicIndex(arr, 0, arr.length - 1);
   }
 
   private int findMagicIndex(int[] arr, int start, int end) {
-    int middle = (start + end) / 2;
-    if (middle == 0 || middle == arr.length - 1) {
-      return arr[middle] == middle ? middle : -1;
+    if (start > end) {
+      return -1;
     }
 
+    int middle = (start + end) / 2;
     if (arr[middle] == middle) {
       return middle;
     } else if (arr[middle] > middle) {
-      return findMagicIndex(arr, start, middle);
+      return findMagicIndex(arr, start, middle - 1);
     } else {
       return findMagicIndex(arr, middle + 1, end);
     }
