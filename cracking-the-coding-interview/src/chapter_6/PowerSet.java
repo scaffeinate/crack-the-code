c5bd16fe80195f4b784f147a98ea4cd4b2099381
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-22 02:06:13 -0400

    Added another Iterative method :thumbsup:

--- cracking-the-coding-interview/src/chapter_6/PowerSet.java
+++ cracking-the-coding-interview/src/chapter_6/PowerSet.java
@@ -1,7 +1,9 @@
 package chapter_6;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 public class PowerSet {
@@ -17,6 +19,36 @@ public class PowerSet {
       result.add(set[i]);
       subsets(set, i + 1, result, subsets);
       result.pop();
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  public List<List<String>> subsetsIterative(String[] set) {
+    List<List<String>> subsets = new ArrayList<List<String>>();
+    Queue<Wrapper> queue = new LinkedList<Wrapper>();
+    queue.add(new Wrapper(new Stack<String>(), 0));
+
+    while (!queue.isEmpty()) {
+      Wrapper wrapper = queue.poll();
+      subsets.add(wrapper.stack);
+
+      for (int i = wrapper.startIndex; i < set.length; i++) {
+        wrapper.stack.add(set[i]);
+        queue.add(new Wrapper((Stack<String>) wrapper.stack.clone(), i + 1));
+        wrapper.stack.pop();
+      }
+    }
+
+    return subsets;
+  }
+
+  class Wrapper {
+    Stack<String> stack = new Stack<String>();
+    int startIndex;
+
+    public Wrapper(Stack<String> stack, int startIndex) {
+      this.stack = stack;
+      this.startIndex = startIndex;
     }
   }
 }
