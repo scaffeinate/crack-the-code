84eec62af2b4f96a09048d491ec4f4c0258c1b64
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-27 02:19:32 -0400

    Added test cases

---
+++ geeks-for-geeks/test/binarytree/TestCheckCousinsBinaryTree.java
@@ -0,0 +1,57 @@
+package binarytree;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import algorithms.util.InputUtil;
+
+public class TestCheckCousinsBinaryTree {
+  private static final String basePath = "input_files/binarytree/check_cousins_binary_tree/";
+  private static String[] testCases = new String[] { "test_case_1", "test_case_2", "test_case_3" };
+  private static List<String[]> inputList = new ArrayList<String[]>();
+  private static CheckCousinsBinaryTree checkBinaryTree = null;
+
+  @BeforeClass
+  public static void setup() {
+    checkBinaryTree = new CheckCousinsBinaryTree();
+    for (String testCase : testCases) {
+      String inputFile = basePath + testCase;
+      inputList.add(InputUtil.readContents(inputFile));
+    }
+  }
+
+  @AfterClass
+  public static void teardown() {
+    checkBinaryTree = null;
+    inputList = null;
+  }
+
+  @Test
+  public void testCheckCousinsBinaryTreeTestCase1() {
+    String[] input = inputList.get(0);
+    assertCheckCousinsBinaryTree(input);
+  }
+
+  @Test
+  public void testCheckCousinsBinaryTreeTestCase2() {
+    String[] input = inputList.get(1);
+    assertCheckCousinsBinaryTree(input);
+  }
+
+  private void assertCheckCousinsBinaryTree(String[] input) {
+    checkBinaryTree.constructTree(input);
+    for (String line : input) {
+      String[] values = line.split(" ");
+      if (values[0].equals("checkCousins")) {
+        assertEquals(Boolean.parseBoolean(values[3]),
+            checkBinaryTree.checkCousins(Integer.parseInt(values[1]), Integer.parseInt(values[2])));
+      }
+    }
+  }
+}
