5d75a8d3982d1ea2b1f9b4bb33bd9ae224496573
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-04-29 02:50:27 -0400

    Added efficient solution to fetch non matching leaves, One of the hardest problem I faced recentyl

--- geeks-for-geeks/src/binarytree/NonMatchingLeavesBinaryTree.java
+++ geeks-for-geeks/src/binarytree/NonMatchingLeavesBinaryTree.java
@@ -1,7 +1,10 @@
 package binarytree;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.Stack;
 
 import datastructures.tree.BinaryTree;
 import datastructures.tree.TreeNode;
@@ -46,5 +49,65 @@ public class NonMatchingLeavesBinaryTree {
       }
       inOrder(root.right, treeNodesList);
     }
+  }
+
+  public int[] findNonMatchingLeaves2(BinaryTree<Integer> tree, BinaryTree<Integer> tree2) {
+    Stack<TreeNode<Integer>> stack = new Stack<TreeNode<Integer>>();
+    Stack<TreeNode<Integer>> stack2 = new Stack<TreeNode<Integer>>();
+    Set<TreeNode<Integer>> visited = new HashSet<TreeNode<Integer>>();
+    Set<TreeNode<Integer>> visited2 = new HashSet<TreeNode<Integer>>();
+
+    stack.push(tree.getRoot());
+    stack2.push(tree2.getRoot());
+
+    while (!stack.isEmpty() && !stack2.isEmpty()) {
+      TreeNode<Integer> peekNode = stack.peek();
+      TreeNode<Integer> peekNode2 = stack2.peek();
+      TreeNode<Integer> node = getNodeToAdd(peekNode, visited);
+      TreeNode<Integer> node2 = getNodeToAdd(peekNode2, visited2);
+
+      if (isLeaf(peekNode) && isLeaf(peekNode2)) {
+        TreeNode<Integer> top = stack.pop();
+        TreeNode<Integer> top2 = stack2.pop();
+
+        visited.add(top);
+        visited2.add(top2);
+
+        if (top.data != top2.data) {
+          return new int[] { top.data, top2.data };
+        }
+      } else if (isLeaf(peekNode)) {
+        pushNodeOrPop(node2, stack2, visited2);
+      } else if (isLeaf(peekNode2)) {
+        pushNodeOrPop(node, stack, visited);
+      } else {
+        pushNodeOrPop(node, stack, visited);
+        pushNodeOrPop(node2, stack2, visited2);
+      }
+    }
+
+    return null;
+  }
+
+  private void pushNodeOrPop(TreeNode<Integer> node, Stack<TreeNode<Integer>> stack, Set<TreeNode<Integer>> visited) {
+    if (node != null) {
+      stack.push(node);
+    } else {
+      visited.add(stack.pop());
+    }
+  }
+
+  private TreeNode<Integer> getNodeToAdd(TreeNode<Integer> node, Set<TreeNode<Integer>> visited) {
+    if (node.left != null && !visited.contains(node.left)) {
+      return node.left;
+    } else if (node.right != null && !visited.contains(node.right)) {
+      return node.right;
+    }
+
+    return null;
+  }
+
+  private boolean isLeaf(TreeNode<Integer> node) {
+    return node.left == null && node.right == null;
   }
 }
