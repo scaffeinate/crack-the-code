a54d139bcc07e5a594be875d7f3edf1cd0a8ae61
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-05-05 23:29:50 -0400

    Added solution for CircularMatrix

---
+++ geeks-for-geeks/src/random/CircularMatrix.java
@@ -0,0 +1,35 @@
+package random;
+
+public class CircularMatrix {
+  public static int[][] constructMatrix(int m, int n) {
+    int[][] result = new int[m][n];
+    int product = m * n;
+    int num = 1, startRow = 0, endRow = m, startColumn = 0, endColumn = n;
+
+    while (num <= product) {
+      for (int j = startColumn; j < endColumn && num <= product; j++) {
+        result[startRow][j] = num++;
+      }
+
+      for (int i = startRow + 1; i < endRow && num <= product; i++) {
+        result[i][endColumn - 1] = num++;
+      }
+
+      for (int j = endColumn - 2; j >= startColumn && num <= product; j--) {
+        result[endRow - 1][j] = num++;
+      }
+
+      for (int i = endRow - 2; i > startRow && num <= product; i--) {
+        result[i][startColumn] = num++;
+      }
+
+      startColumn++;
+      startRow++;
+      endColumn--;
+      endRow--;
+
+    }
+
+    return result;
+  }
+}
