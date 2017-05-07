da84d512626f2c2bc4ce0f223816d3dd6bface4d
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-05-07 14:58:41 -0400

    Added EightQueens backtracking solution

--- cracking-the-coding-interview/src/chapter_6/EightQueens.java
+++ cracking-the-coding-interview/src/chapter_6/EightQueens.java
@@ -2,22 +2,31 @@ package chapter_6;
 
 public class EightQueens {
   public void arrangeQueens(int[][] chessBoard) {
-    System.out.println(canPlaceQueen(6, 7, chessBoard));
+    arrange8Queens(0, 0, chessBoard);
   }
 
-  private boolean arrange8Queens(int row, int column, int n, int[][] chessBoard) {
+  private boolean arrange8Queens(int column, int n, int[][] chessBoard) {
+    if (n == chessBoard.length) {
+      return true;
+    }
+
+    for (int i = 0; i < chessBoard.length; i++) {
+      if (canPlaceQueen(i, column, chessBoard)) {
+        chessBoard[i][column] = 1;
+        if (arrange8Queens(column + 1, n + 1, chessBoard)) {
+          return true;
+        } else {
+          chessBoard[i][column] = 0;
+        }
+      }
+    }
+
     return false;
   }
 
   private boolean canPlaceQueen(int row, int column, int[][] chessBoard) {
-    for (int i = 0; i < chessBoard[0].length; i++) {
+    for (int i = 0; i < column; i++) {
       if (chessBoard[row][i] == 1) {
-        return false;
-      }
-    }
-
-    for (int i = 0; i < chessBoard.length; i++) {
-      if (chessBoard[i][column] == 1) {
         return false;
       }
     }
@@ -28,7 +37,7 @@ public class EightQueens {
       }
     }
 
-    for (int i = row, j = column; i < chessBoard.length && j < chessBoard[0].length; i++, j++) {
+    for (int i = row, j = column; i < chessBoard.length && j >= 0; i++, j--) {
       if (chessBoard[i][j] == 1) {
         return false;
       }
@@ -36,13 +45,23 @@ public class EightQueens {
 
     return true;
   }
-  
+
+  private void printBoard(int[][] chessBoard) {
+    for (int i = 0; i < chessBoard.length; i++) {
+      for (int j = 0; j < chessBoard[0].length; j++) {
+        System.out.print(chessBoard[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
   public static void main(String[] args) {
     EightQueens eightQueens = new EightQueens();
-    int[][] chessBoard = new int[][] { new int[] { 1, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 1, 0, 0, 0, 0, 0, 0 },
-      new int[] { 0, 0, 1, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 1, 0, 0, 0, 0 },
-      new int[] { 0, 0, 0, 0, 1, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 1, 0, 0 },
-      new int[] { 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0 } };
+    int[][] chessBoard = new int[][] { new int[] { 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[] { 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[] { 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+        new int[] { 0, 0, 0, 0, 0, 0, 0, 0 }, new int[] { 0, 0, 0, 0, 0, 0, 0, 0 } };
     eightQueens.arrangeQueens(chessBoard);
+    eightQueens.printBoard(chessBoard);
   }
 }
