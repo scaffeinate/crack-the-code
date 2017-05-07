7b41bea628727b31b795b40dbebdbe43deb131ef
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-05-07 01:16:28 -0400

    Added the right solution for Coins and Test Cases

--- cracking-the-coding-interview/src/chapter_6/Coins.java
+++ cracking-the-coding-interview/src/chapter_6/Coins.java
@@ -5,22 +5,72 @@ import java.util.Map;
 
 public class Coins {
   public long numWays(int n) {
-    return numWaysMemoized(n, new int[] { 25, 10, 5, 1 }, new HashMap<Integer, Long>());
+    Map<Tuple, Long> map = new HashMap<Tuple, Long>();
+    long numWays = numWaysMemoized(n, 0, new int[] { 25, 10, 5, 1 }, map);
+    return numWays;
   }
 
-  private long numWaysMemoized(int n, int[] coins, Map<Integer, Long> resultsMap) {
+  private long numWaysMemoized(int n, int index, int[] coins, Map<Tuple, Long> resultsMap) {
     if (n == 0) {
       return 1;
-    } else if (n < 0) {
-      return 0;
-    } else if (!resultsMap.containsKey(n)) {
-      long sum = 0;
-      for (int coin : coins) {
-        sum += numWaysMemoized(n - coin, coins, resultsMap);
-      }
-      resultsMap.put(n, sum);
     }
 
-    return resultsMap.get(n);
+    if (index >= coins.length) {
+      return 0;
+    } else {
+      Tuple tuple = new Tuple(coins[index], n);
+      if (!resultsMap.containsKey(tuple)) {
+        long sum = 0;
+        int numWays = (n / coins[index]);
+        for (int i = 0; i <= numWays; i++) {
+          sum += numWaysMemoized(n - (i * coins[index]), index + 1, coins, resultsMap);
+        }
+        resultsMap.put(tuple, sum);
+      }
+
+      return resultsMap.get(tuple);
+    }
+  }
+
+  class Tuple {
+    int coin;
+    int n;
+
+    Tuple(int coin, int n) {
+      this.coin = coin;
+      this.n = n;
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + getOuterType().hashCode();
+      result = prime * result + coin;
+      result = prime * result + n;
+      return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (obj == null)
+        return false;
+      if (getClass() != obj.getClass())
+        return false;
+      Tuple other = (Tuple) obj;
+      if (!getOuterType().equals(other.getOuterType()))
+        return false;
+      if (coin != other.coin)
+        return false;
+      if (n != other.n)
+        return false;
+      return true;
+    }
+
+    private Coins getOuterType() {
+      return Coins.this;
+    }
   }
 }
