905841f0d4905d76c97455600e28b496bb2415d5
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-05-11 00:58:22 -0400

    Added a way to trace back the actions

--- algorithms/src/algorithms/dp/Levenshtein.java
+++ algorithms/src/algorithms/dp/Levenshtein.java
@@ -1,5 +1,8 @@
 package algorithms.dp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Levenshtein {
 
   private String s1, s2;
@@ -30,6 +33,32 @@ public class Levenshtein {
       }
     }
     return matrix[s1.length()][s2.length()];
+  }
+
+  public List<String> getActions() {
+    List<String> actions = new ArrayList<String>();
+    int i = matrix.length - 1, j = matrix[0].length - 1;
+    while (i > 0 && j > 0) {
+      if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+        i--;
+        j--;
+      } else {
+        int current = matrix[i][j];
+        if (current == (matrix[i - 1][j - 1] + 1)) {
+          actions.add(new String("modify " + s2.charAt(j - 1) + " to " + s1.charAt(i - 1)));
+          i--;
+          j--;
+        } else if (current == (matrix[i][j - 1] + 1)) {          
+          actions.add(new String("delete " + s2.charAt(j - 1)));
+          j--;
+        } else if (current == (matrix[i - 1][j] + 1)) {
+          actions.add(new String("add " + s1.charAt(i - 1)));
+          i--;
+        }
+      }
+    }
+
+    return actions;
   }
 
   private int minOf(int... numbers) {
