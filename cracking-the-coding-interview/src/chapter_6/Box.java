13536a8fd877811ba36da04a06ffd60b0bdbe501
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-05-17 01:54:56 -0400

    Added memoized solution to StackOfBoxes

---
+++ cracking-the-coding-interview/src/chapter_6/Box.java
@@ -0,0 +1,50 @@
+package chapter_6;
+
+public class Box {
+  public int width = 0, height = 0, depth = 0;
+
+  public Box(int width, int height, int depth) {
+    this.width = width;
+    this.height = height;
+    this.depth = depth;
+  }
+
+  public boolean greaterThan(Box box) {
+    return (this.width >= box.width && this.height >= box.height && this.depth >= box.depth);
+  }
+  
+  
+
+  @Override
+  public String toString() {
+    return "Box [width=" + width + ", height=" + height + ", depth=" + depth + "]";
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + depth;
+    result = prime * result + height;
+    result = prime * result + width;
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Box other = (Box) obj;
+    if (depth != other.depth)
+      return false;
+    if (height != other.height)
+      return false;
+    if (width != other.width)
+      return false;
+    return true;
+  }
+}
---
+++ cracking-the-coding-interview/src/chapter_6/StackOfBoxes.java
@@ -0,0 +1,47 @@
+package chapter_6;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StackOfBoxes {
+  public int maximumHeight(List<Box> boxes) {
+    int maxHeight = 0;
+    Collections.sort(boxes, new Comparator<Box>() {
+      @Override
+      public int compare(Box o1, Box o2) {
+        return Integer.valueOf(o2.height).compareTo(Integer.valueOf(o1.height));
+      }
+    });
+
+    Map<Box, Integer> results = new HashMap<Box, Integer>();
+
+    for (int i = 0; i < boxes.size(); i++) {
+      Box box = boxes.get(i);
+      int height = (!results.containsKey(box)) ? maximumHeight(boxes, i, results) + 1 : results.get(box);
+      maxHeight = Math.max(height, maxHeight);
+    }
+    
+    return maxHeight;
+  }
+
+  private int maximumHeight(List<Box> boxes, int index, Map<Box, Integer> results) {
+    if (index == boxes.size()) {
+      return 0;
+    }
+
+    int maxHeight = 0;
+    for (int i = index + 1; i < boxes.size(); i++) {
+      Box box = boxes.get(i);
+      if (boxes.get(index).greaterThan(box)) {
+        int height = (!results.containsKey(box)) ? maximumHeight(boxes, i, results) + 1 : results.get(box);
+        results.putIfAbsent(box, height);
+        maxHeight = Math.max(maxHeight, height);
+      }
+    }
+
+    return maxHeight;
+  }
+}
