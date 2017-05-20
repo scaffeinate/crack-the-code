9c42c8adf5f00db94d977ff7772f1c07ae2cc665
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-05-20 01:23:30 -0400

    Added solution for LongestPalindromicSubstring set2 without using additional memory

---
+++ geeks-for-geeks/src/random/LongestPalindromicSubstring.java
@@ -0,0 +1,29 @@
+package random;
+
+/**
+ * Created by sudharti on 5/20/17.
+ */
+public class LongestPalindromicSubstring {
+  public String longestPS(String input) {
+    int maxLength = 1, start = 0;
+    for (int i = 0; i < input.length(); i++) {
+      for (int x = i - 1, y = i + 1; x >= 0 && y < input.length() && input.charAt(x) == input.charAt(y); x--, y++) {
+        int length = y - x + 1;
+        if (length > maxLength) {
+          maxLength = length;
+          start = x;
+        }
+      }
+
+      for(int x = i, y = i+1; x >= 0 && y < input.length() && input.charAt(x) == input.charAt(y); x--, y++) {
+        int length = y - x + 1;
+        if (length > maxLength) {
+          maxLength = length;
+          start = x;
+        }
+      }
+    }
+
+    return input.substring(start, (start + maxLength));
+  }
+}
