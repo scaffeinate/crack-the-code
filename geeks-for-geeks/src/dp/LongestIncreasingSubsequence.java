1305c012a8f7eed3814328d742ad2b1e2bb94782
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-06-22 11:37:39 -0400

    4 spaces

--- geeks-for-geeks/src/dp/LongestIncreasingSubsequence.java
+++ geeks-for-geeks/src/dp/LongestIncreasingSubsequence.java
@@ -5,46 +5,46 @@ package dp;
  * Solution: https://www.youtube.com/watch?v=CE2b_-XfVDk
  */
 public class LongestIncreasingSubsequence {
-	public int longestSeq(int[] arr) {
-		int max = 0;
-		for (int i = 0; i < arr.length - 1; i++) {
-			max = Math.max(max, 1 + longestSeq(arr, arr[i], i + 1));
-		}
+    public int longestSeq(int[] arr) {
+        int max = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            max = Math.max(max, 1 + longestSeq(arr, arr[i], i + 1));
+        }
 
-		return max;
-	}
+        return max;
+    }
 
-	private int longestSeq(int[] arr, int prev, int index) {
-		if (index == arr.length) {
-			return 0;
-		}
+    private int longestSeq(int[] arr, int prev, int index) {
+        if (index == arr.length) {
+            return 0;
+        }
 
-		if (arr[index] < prev) {
-			return longestSeq(arr, prev, index + 1);
-		} else {
-			return Math.max(1 + longestSeq(arr, arr[index], index + 1), longestSeq(arr, prev, index + 1));
-		}
-	}
+        if (arr[index] < prev) {
+            return longestSeq(arr, prev, index + 1);
+        } else {
+            return Math.max(1 + longestSeq(arr, arr[index], index + 1), longestSeq(arr, prev, index + 1));
+        }
+    }
 
-	public int longestSeqDP(int[] arr) {
-		int[] memo = new int[arr.length];
-		int[] trace = new int[arr.length];
-		int j = 1;
-		while (j < arr.length) {
-			for (int i = 0; i < j; i++) {
-				if ((arr[i] < arr[j]) && (memo[i] + 1 > memo[j])) {
-					memo[j] = memo[i] + 1;
-					trace[j] = i;
-				}
-			}
-			j++;
-		}
+    public int longestSeqDP(int[] arr) {
+        int[] memo = new int[arr.length];
+        int[] trace = new int[arr.length];
+        int j = 1;
+        while (j < arr.length) {
+            for (int i = 0; i < j; i++) {
+                if ((arr[i] < arr[j]) && (memo[i] + 1 > memo[j])) {
+                    memo[j] = memo[i] + 1;
+                    trace[j] = i;
+                }
+            }
+            j++;
+        }
 
-		int max = memo[0];
-		for (int i = 1; i < memo.length; i++) {
-			max = Math.max(max, memo[i]);
-		}
+        int max = memo[0];
+        for (int i = 1; i < memo.length; i++) {
+            max = Math.max(max, memo[i]);
+        }
 
-		return max + 1;
-	}
+        return max + 1;
+    }
 }
