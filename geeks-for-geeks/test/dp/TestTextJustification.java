51a186f0f77f6c9a0300743d1337845113ac8395
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-07-27 02:13:02 -0400

    Add couple more test cases

--- geeks-for-geeks/test/dp/TestTextJustification.java
+++ geeks-for-geeks/test/dp/TestTextJustification.java
@@ -16,7 +16,8 @@ import static org.junit.Assert.assertEquals;
 public class TestTextJustification {
     private static String basePath = "input_files/dp/text_justification/";
     private static List<String[]> inputList = new ArrayList<>();
-    private static String[] testCases = new String[]{"test_case_1", "test_case_2", "test_case_3", "test_case_4"};
+    private static String[] testCases = new String[]{"test_case_1", "test_case_2", "test_case_3", "test_case_4",
+            "test_case_5", "test_case_6"};
     private static TextJustification textJustification = null;
 
     @BeforeClass
@@ -54,13 +55,23 @@ public class TestTextJustification {
         assertTextJustification(inputList.get(3));
     }
 
+    @Test
+    public void testTextJustificationTestCase5() {
+        assertTextJustification(inputList.get(4));
+    }
+
+    @Test
+    public void testTextJustificationTestCase6() {
+        assertTextJustification(inputList.get(5));
+    }
+
 
     private void assertTextJustification(String[] input) {
         String[] inputArr = input[0].split(" ");
         String[] words = inputArr[0].split(",");
         int W = Integer.parseInt(inputArr[1]);
         StringBuilder builder = new StringBuilder();
-        for(int i = 1; i < input.length; i++) {
+        for (int i = 1; i < input.length; i++) {
             builder.append(input[i]).append("\n");
         }
 
