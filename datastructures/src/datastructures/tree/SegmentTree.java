b4a12b8c062ca89d033bc48c36ad4f43f7471012
Sudharsanan Muralidharan <sudhar.ti@gmail.com>
2017-08-23 01:31:44 -0400

    update needs to follow different logic

--- datastructures/src/datastructures/tree/SegmentTree.java
+++ datastructures/src/datastructures/tree/SegmentTree.java
@@ -1,11 +1,13 @@
 package datastructures.tree;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by sudharti on 8/22/17.
  */
 public class SegmentTree<T> {
 
-    private SegmentTreeNode<T>[] tree;
+    private T[] tree;
     private T[] array;
     private TreeType type;
     private Class clazz;
@@ -20,7 +22,7 @@ public class SegmentTree<T> {
         clazz = array.getClass().getComponentType();
         this.type = type;
         this.array = array;
-        this.tree = new SegmentTreeNode[treeSize()];
+        this.tree = (T[]) Array.newInstance(clazz, treeSize());
     }
 
     public void constructTree() {
@@ -28,26 +30,18 @@ public class SegmentTree<T> {
     }
 
     public T rangeOf(int start, int end) {
-        return rangeOf(array, tree, start, end, 0);
+        return rangeOf(tree, 0, array.length - 1, start, end, 0);
     }
 
-    private T rangeOf(T[] array, SegmentTreeNode<T>[] tree, int start, int end, int pos) {
-        if (start <= tree[pos].start && end >= tree[pos].end) {
-            return tree[pos].val;
-        } else if (end < tree[pos].start || start > tree[pos].end) {
-            return defaultValue();
-        } else {
-            return eval(rangeOf(array, tree, start, end, (2 * pos) + 1),
-                    rangeOf(array, tree, start, end, (2 * pos) + 2));
-        }
+    public void update(int index, T updatedVal) {
+        T diff = diff(updatedVal, array[index]);
+        tree[index] = updatedVal;
+        update(tree, 0, array.length - 1, index, diff, 0);
     }
 
-    private void constructTree(T[] array, SegmentTreeNode<T>[] tree, int start, int end, int pos) {
-        tree[pos] = new SegmentTreeNode<>();
+    private void constructTree(T[] array, T[] tree, int start, int end, int pos) {
         if (start == end) {
-            tree[pos].val = array[start];
-            tree[pos].start = start;
-            tree[pos].end = end;
+            tree[pos] = array[start];
             return;
         }
 
@@ -58,14 +52,36 @@ public class SegmentTree<T> {
         constructTree(array, tree, middle + 1, end, child + 2);
 
         tree[pos] = tree[pos];
-        tree[pos].val = eval(tree[child + 1].val, tree[child + 2].val);
-        tree[pos].start = start;
-        tree[pos].end = end;
+        tree[pos] = eval(tree[child + 1], tree[child + 2]);
     }
 
+    private T rangeOf(T[] tree, int start, int end, int rangeStart, int rangeEnd, int pos) {
+        if (rangeStart <= start && rangeEnd >= end) {
+            return tree[pos];
+        } else if (rangeEnd < start || rangeEnd > end) {
+            return defaultValue();
+        } else {
+            int middle = (start + end) / 2;
+            return eval(rangeOf(tree, start, middle, rangeStart, rangeEnd, (2 * pos) + 1),
+                    rangeOf(tree, middle + 1, end, rangeStart, rangeEnd, (2 * pos) + 2));
+        }
+    }
+
+    private void update(T[] tree, int start, int end, int index, T diff, int pos) {
+        if (index < start || index > end) {
+            return;
+        }
+
+        tree[pos] = add(tree[pos], diff);
+        if (start != end) {
+            int middle = (start + end) / 2;
+            update(tree, start, middle, index, diff, (2 * pos) + 1);
+            update(tree, middle + 1, end, index, diff, (2 * pos) + 2);
+        }
+    }
 
     private T eval(T t1, T t2) {
-        if (t1 instanceof Integer) {
+        if (clazz.equals(Integer.class)) {
             if (type == TreeType.MIN) {
                 return ((Integer) t1).compareTo((Integer) t2) < 0 ? t1 : t2;
             } else if (type == TreeType.MAX) {
@@ -73,7 +89,7 @@ public class SegmentTree<T> {
             } else {
                 return (T) Integer.valueOf(((Integer) t1) + ((Integer) t2));
             }
-        } else if (t1 instanceof Double) {
+        } else if (clazz.equals(Double.class)) {
             if (type == TreeType.MIN) {
                 return ((Double) t1).compareTo((Double) t2) < 0 ? t1 : t2;
             } else if (type == TreeType.MAX) {
@@ -81,7 +97,7 @@ public class SegmentTree<T> {
             } else {
                 return (T) Double.valueOf(((Double) t1) + ((Double) t2));
             }
-        } else if (t1 instanceof Long) {
+        } else if (clazz.equals(Long.class)) {
             if (type == TreeType.MIN) {
                 return ((Long) t1).compareTo((Long) t2) < 0 ? t1 : t2;
             } else if (type == TreeType.MAX) {
@@ -116,6 +132,26 @@ public class SegmentTree<T> {
         }
     }
 
+    private T add(T t1, T t2) {
+        if (clazz.equals(Integer.class)) {
+            return (T) (Integer.valueOf(((Integer) t1) + ((Integer) t2)));
+        } else if (clazz.equals(Double.class)) {
+            return (T) (Double.valueOf(((Double) t1) + ((Double) t2)));
+        } else {
+            return (T) (Long.valueOf(((Long) t1) + ((Long) t2)));
+        }
+    }
+
+    private T diff(T t1, T t2) {
+        if (clazz.equals(Integer.class)) {
+            return (T) (Integer.valueOf(((Integer) t1) - ((Integer) t2)));
+        } else if (clazz.equals(Double.class)) {
+            return (T) (Double.valueOf(((Double) t1) - ((Double) t2)));
+        } else {
+            return (T) (Long.valueOf(((Long) t1) - ((Long) t2)));
+        }
+    }
+
     private int treeSize() {
         int len = array.length;
         if ((len & len - 1) == 0) {
@@ -129,14 +165,11 @@ public class SegmentTree<T> {
         }
     }
 
-    public static class SegmentTreeNode<T> {
-        T val;
-        int start = 0, end = 0;
-    }
-
     public static void main(String[] args) {
-        SegmentTree<Integer> tree = new SegmentTree<>(TreeType.MIN, new Integer[]{0, 2, 3, 4, 4, 2, 3, 10});
+        SegmentTree<Integer> tree = new SegmentTree<>(TreeType.MAX, new Integer[]{1, 3, 5, 7, 9, 11});
         tree.constructTree();
-        System.out.println(tree.rangeOf(0, 9));
+        System.out.println(tree.rangeOf(0, 5));
+        tree.update(3, -5);
+        System.out.println(tree.rangeOf(0, 5));
     }
 }
